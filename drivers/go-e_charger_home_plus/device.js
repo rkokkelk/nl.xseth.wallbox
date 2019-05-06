4acd3f4b2a18b0d22547c4c85332b59f341facc2
phatenl <37834463+phatenl@users.noreply.github.com>
2019-05-06 21:38:29 +0200

    no idea what i'm doing yet

--- drivers/go-e_charger_home_plus/device.js
+++ drivers/go-e_charger_home_plus/device.js
@@ -8,6 +8,25 @@ class goe_charger_home_plus_device extends Homey.Device {
 		this.log('goe_charger_home_plus_device has been inited');
 	}
 
-}
+	async onInit() {
+			this.log('device init');
+			let settings = this.getSettings();
+			let name = this.getName() + '_' + this.getData().id;
+			} // end onInit
 
+			onAdded() {
+	        let id = this.getData().id;
+	        this.log('device added: ', id);
+
+	    } // end onAdded
+
+	    onDeleted() {
+
+	        let id = this.getData().id;
+	        clearInterval(this.pollingIntervalCurrent);
+	        this.log('device deleted:', id);
+
+	    } // end onDeleted
+
+}
 module.exports = goe_charger_home_plus_device;
--- drivers/go-e_charger_home_plus/driver.js
+++ drivers/go-e_charger_home_plus/driver.js
@@ -8,6 +8,31 @@ class goe_charger_home_plus extends Homey.Driver {
 		this.log('go-e_charger_home_plus has been inited');
 	}
 
-}
+	onPair(socket) {
 
+			let devices = [
+					{
+							"name": "My Device",
+							"data": { "id": "abcd" },
+							"settings": {
+									"APIKey": 'APIKey',
+									"IPaddress": '192.168.1.45'
+							}
+					}
+			]
+
+			// this is called when the user presses save settings button in pair.html
+			socket.on('get_devices', (device_data, callback) => {
+					devices = device_data;
+					callback(null, devices);
+			});
+
+			// this happens when user clicks away the pairing windows
+			socket.on('disconnect', () => {
+					this.log("OpenWeatherMap - Pairing is finished (done or aborted) ");
+			})
+
+	} // end onPair
+
+}
 module.exports = goe_charger_home_plus;
