8e1dd0fe0b7732d059935fa4031ded54ac86ffae
phatenl <37834463+phatenl@users.noreply.github.com>
2019-05-06 22:17:12 +0200

    problem on list devices in app.json

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 
 const Homey = require('homey');
 
-class MyApp extends Homey.App {
-	
+class goechargerapp extends Homey.App {
+
 	onInit() {
 		this.log('MyApp is running...');
 	}
-	
+
 }
 
-module.exports = MyApp;
+module.exports = goechargerapp;
