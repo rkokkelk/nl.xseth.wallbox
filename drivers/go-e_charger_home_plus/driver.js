a09188c4e0100efd11ec2fb916fbfb100e488798
phatenl <37834463+phatenl@users.noreply.github.com>
2019-05-07 20:55:30 +0200

    1

--- drivers/go-e_charger_home_plus/driver.js
+++ drivers/go-e_charger_home_plus/driver.js
@@ -5,22 +5,33 @@ const Homey = require('homey');
 class goe_charger_home_plus extends Homey.Driver {
 
 	onInit() {
-		this.log('go-e_charger_home_plus has been inited');
+		this.log('goe_charger_home_plus has been inited');
 	}
 
-	onPair(socket) {
+	onPair( socket ) {
+    const devices = [
+      {
+        'name': 'goe_charger_home_plus',
+        'data': {
+          'id': 'abcd',
+        }
+      }
+    ]
 
-			socket.on('get_devices', (device_data, callback) => {
-					devices = device_data;
-					callback(null, devices);
-			});
+    socket.on('list_devices', function( data, callback ) {
+      // emit when devices are still being searched
+      socket.emit('list_devices', devices );
 
-			// this happens when user clicks away the pairing windows
-			socket.on('disconnect', () => {
-					this.log("go-e_charger_home_plus - Pairing is finished (done or aborted) ");
-			})
+      // fire the callback when searching is done
+      callback( null, devices );
 
-	} // end onPair
+      // when no devices are found, return an empty array
+      // callback( null, [] );
 
+      // or fire a callback with Error to show that instead
+      // callback( new Error('Something bad has occured!') );
+    });
+  }
 }
+
 module.exports = goe_charger_home_plus;
