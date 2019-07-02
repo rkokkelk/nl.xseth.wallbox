926191a5930dfc0dc04aaf9749963762c21ab752
phatenl <37834463+phatenl@users.noreply.github.com>
2019-07-02 20:13:33 +0200

    minor change

--- api/api.js
+++ api/api.js
@@ -56,83 +56,6 @@ module.exports = class goechargerApi {
             charge_amp: Number(goecharger.amp),
             charge_amp_limit: Number(goecharger.ama)//,
 
-//GET-able values:
-//version = encryption B:off C:on
-//rbc = reboot counter
-//rbt = reboot timer
-//car = STATUS 1:ready_no_car_connected 2:car_charging 3:waiting_for_car_to_approve 4:charing_finished_car_still_connected
-//err = ERROR code 1:RCCB 3:phase_error 8:no_ground_error 10:other
-//cbl
-//pha
-//tmp
-//dws
-//adi
-//uby
-//eto
-//wst
-//nrg
-//fwv
-//sse
-//eca
-//ecr
-//ecd
-//ec4
-//ec5
-//ec6
-//ec7
-//ec8
-//ec9
-//ec1
-//rca
-//rcr
-//rcd
-//rc4
-//rc5
-//rc6
-//rc7
-//rc8
-//rc9
-//rc1
-
-//SET-able values:
-//amp = AMPERE value for Charging 6-32 (max AMP limited by ama) (CHANGE THIS FOR LOAD BALANCING PURPOSES)
-//ast = ACCESS STATE 0:open_anyone_can_charge 1:RFID_or_APP_required_to_open 2:powerprice_or_automatic
-//alw
-//stp
-//dwo
-//wss
-//wke
-//wen
-//tof
-//tds
-//lbr
-//aho
-//afi}
-//ama = AMPERE absolute max value (!! DANGER DO NOT INCREASE THIS UNLESS YOU'RE CERTAIN THE HOME INFRASTRUCTURE CAN SUPPORT IT)
-//al1
-//al2
-//al3
-//al4
-//al5
-//cid
-//cch
-//cfi
-//lse
-//ust
-//wak
-//r1x
-//dto
-//nmo
-//rna
-//rnm
-//rne
-//rn4
-//rn5
-//rn6
-//rn7
-//rn8
-//rn9
-//rn1
         };
     }
 
@@ -142,7 +65,7 @@ module.exports = class goechargerApi {
         try {
             console.log('values: ' + values);
             console.log('POST to: /mqtt?payload=');
-            const res = await Fetch('http://' + this._ip + '/mqtt?payload=' + values, {method: 'GET'});
+            const res = await Fetch('http://' + this._ip + '/mqtt?payload=' + values);
             if (res.status === 200) {
                 return Promise.resolve(res);
             } else {
