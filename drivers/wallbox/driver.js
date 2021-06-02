f424987712e9255635b52a71e04b8745cbc286d3
RoyK <rkokk@protonmail.com>
2021-06-02 21:33:41 +0200

    feat: working pairing mechanism

--- drivers/wallbox/driver.js
+++ drivers/wallbox/driver.js
@@ -5,29 +5,43 @@ const WallboxAPI = require('/lib/wallbox_api');
 
 class goe_charger_home_plus extends Homey.Driver {
 
-	onInit() {
-		this.log('goe_charger_home_plus has been inited');
-	}
+	async onPair(session) {
+		let user = "";
+    let pass = "";
+		let api = null;
 
-	async onPair(socket) {
-			this.log('pairing started');
-			socket.on('testConnection', async (data, callback) => {
-					try {
-					    let user = data['user'];
-					    let pass = data['pass'];
-							this.log(user+ ' ' + pass);
-							const api = new WallboxAPI(user, pass);
-						  await api.authenticate();
-						  const chargers = await api.getChargers();
-							this.log(chargers);
-							this.log(chargers['result']['groups']);
-							this.log(chargers['result']['groups'][0]['chargers']);
-							callback(false, chargers);
-					} catch (e) {
-							console.log(e);
-							callback(true, e);
-					}
+    session.setHandler("login", async (data) => {
+      user = data.username;
+      pass = data.password;
+
+			api = new WallboxAPI(user, pass);
+			await api.authenticate();
+
+      // return true to continue adding the device if the login succeeded
+      // return false to indicate to the user the login attempt failed
+      // thrown errors will also be shown to the user
+      return true;
+    });
+
+    session.setHandler("list_devices", async () => {
+			const chargers = await api.getChargers();
+			const chargerDevices = chargers['result']['groups'].map((charger) => {
+				this.log("Found charger: " + charger);
+
+				return {
+					name: charger['chargers'][0]['name'],
+					data: {
+						id: charger['id']
+					},
+					settings: {
+						user: user,
+						pass: pass
+					},
+				}
 			});
+
+			return chargerDevices
+		});
 	}
 }
 
