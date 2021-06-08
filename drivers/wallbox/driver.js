4368f61d977c17b17a962702db30987d0dcaf0ae
RoyK <roy.kokkelkoren@ncsc.nl>
2021-06-08 17:07:34 +0200

    feat: add driver init for device trigger cards

--- drivers/wallbox/driver.js
+++ drivers/wallbox/driver.js
@@ -5,11 +5,43 @@ const WallboxAPI = require('/lib/wallbox_api');
 
 class goe_charger_home_plus extends Homey.Driver {
 
+  async onInit() {
+    console.log("onInit WallBox driver");
+
+    this._triggers = {};
+    this.trigger_keys = [
+      'charging_ended', 'charging_started', 'car_connected', 'car_unplugged', 'status_changed'
+    ]
+      '
+    for (const type of this.trigger_keys) {
+      this._triggers[type] = this.homey.flow.getDeviceTriggerCard(type);
+    }
+  }
+
+  trigger(key, device, tokens, state){
+    /**
+     * Trigger a triggerCard
+     *
+     * @param {String} key: id of triggerCard to trigger
+     * @param {Device} device: device for which to trigger card
+     * @param {Dictionary} tokens: list of related tokens
+     * @param {Dictionary} state: current state of trigger
+     */
+
+    this._triggers[key]
+      .trigger(device, tokens, state)
+      .then(this.log)
+      .catch(this.error);
+  }
+
+
+
 	async onPair(session) {
 		let user = "";
     let pass = "";
 		let api = null;
 
+    // Pairing sequences
     session.setHandler("login", async (data) => {
       user = data.username;
       pass = data.password;
