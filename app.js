352ddba964a73c728f2104e4cf4031d07506ba5b
RoyK <rkokk@protonmail.com>
2023-03-26 15:01:43 +0200

    feat: add inspector activation for development

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const inspector = require('inspector');
 const Homey = require('homey');
 
 if (process.env.DEBUG === '1') {
@@ -9,6 +10,10 @@ if (process.env.DEBUG === '1') {
 class wallboxapp extends Homey.App {
 
 	onInit() {
+
+		if (process.env.DEBUG === '1')
+			inspector.open(8080, '0.0.0.0', true)
+
 		this.log('wallboxapp is running...');
 	}
 }
